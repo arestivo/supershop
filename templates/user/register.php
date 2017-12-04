45728e0d6e1a82dd5f6011d4ddbb3c4b68bec504
Andre Restivo <andre.restivo@gmail.com>
2017-12-04 23:45:29 +0000

    Code cleaning

--- templates/user/register.php
+++ templates/user/register.php
@@ -1,9 +1,15 @@
 <section id="register">
   <h2>Register</h2>
   <form action="action_register.php" method="post">
-    <input type="text" name="username" placeholder="Username" required><span class="hint">Only lowercase, at least 3 characters</span>
-    <input type="password" name="password" placeholder="Password" required><span class="hint">One uppercase, 1 symbol, 1 number, at least 8 characters</span>
-    <input type="password" name="repeat" placeholder="Repeat Password" required><span class="hint">Must match password</span>
+    <input type="text" name="username" placeholder="Username" required>
+    <span class="hint">Only lowercase, at least 3 characters</span>
+
+    <input type="password" name="password" placeholder="Password" required>
+    <span class="hint">One uppercase, 1 symbol, 1 number, at least 8 characters</span>
+
+    <input type="password" name="repeat" placeholder="Repeat Password" required>
+    <span class="hint">Must match password</span>
+
     <input type="submit" value="Register">
   </form>
 </section>
